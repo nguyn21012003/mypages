8bbd522767a47a4bdc8e029bb547808f16cd607c
nguyn21012003 <nguyendeptrai980@gmail.com>
2024-08-03 14:54:37 +0700

    updae footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -5,11 +5,12 @@ import { PiCopyright } from "react-icons/pi";
 import { FiGithub, FiFacebook } from "react-icons/fi";
 import { AiOutlineGlobal } from "react-icons/ai";
 import { CiLinkedin } from "react-icons/ci";
-*/
-
 import { FiGithub, FiFacebook } from "react-icons/fi";
 import { AiOutlineGlobal } from "react-icons/ai";
 import { CiLinkedin } from "react-icons/ci";
+*/
+
+
 export const MyFooter: React.FC = () => {
   const year = new Date().getFullYear();
   return (
