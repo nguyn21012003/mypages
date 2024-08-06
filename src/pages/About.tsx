c12c78ca47eff000d1440de41adbe80956a3414b
nguyn21012003 <nguyendeptrai980@gmail.com>
2024-08-06 13:47:15 +0700

    update theme and more

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,121 +1,136 @@
 import React from "react";
 
-import { HiAcademicCap } from "react-icons/hi";
-import Trimmed from "../component/trimmed";
+import { HiOutlineDocumentDownload } from "react-icons/hi";
+import { FiGithub, FiFacebook } from "react-icons/fi";
+import { AiOutlineGlobal } from "react-icons/ai";
+import { CiLinkedin } from "react-icons/ci";
+
 import Skills from "../component/skill";
-import RecentlyPost from "../component/recently";
+import { RecentlyPost } from "../component/recently";
+import MyExperience from "../component/experience";
 
 export const About: React.FC = () => {
   return (
-    <div className="w-full space-y-[6rem]">
-      <div className="flex items-center justify-center">
-        <div className="bg-transparent sm:backdrop-blur-none md:max-w-[50rem] md:backdrop-blur-sm lg:backdrop-blur-sm 2xl:backdrop-blur-sm">
-          <div className="md:flex">
-            <div className="p-8 text-white dark:text-black md:shrink-0">
-              <img
-                alt="Khoi Nguyen Tran"
-                className="size-20 self-start rounded-full border-[4px]"
-                src={Logo}
-              ></img>
-              <p>
-                <a className="font-Poppins text-lg font-semibold">
-                  Khoi Nguyen Tran
-                </a>
-              </p>
-              <p>
-                <a className="font-Poppins text-lg font-semibold">
-                  Dep. of Theoretical Physics
-                </a>
-              </p>
-              <p>
-                <a className="font-Poppins text-lg font-semibold">
-                  CONTACT ME AT
-                </a>
-              </p>
-              <div className="flex space-x-2">
-                <a
-                  href="https://jeremy-tran.vercel.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <div className="hover:scale-110">
-                      <AiOutlineGlobal />
-                    </div>
+    <div className="w-full select-none">
+      <div className="space-y-[20rem] lg:ml-[10rem] xl:ml-10">
+        <div className="flex items-center justify-center">
+          <div>
+            <div className="mt-14 bg-transparent md:max-w-[50rem]">
+              <div className="md:flex">
+                <div className="p-8 md:shrink-0">
+                  <img
+                    alt="Khoi Nguyen Tran"
+                    className="size-20 self-start rounded-full border-[4px]"
+                    src="/me.jpg"
+                  ></img>
+                  <span className="relative flex h-3 w-3">
+                    <span className="absolute -right-14 -top-[1rem] inline-flex h-full w-full animate-ping rounded-full bg-green-300 opacity-75"></span>
+                    <span className="relative -right-14 -top-[1rem] inline-flex h-3 w-3 rounded-full bg-green-400"></span>
+                  </span>
+                  <p>
+                    <a className="text-lg font-semibold text-[#0b1215] dark:text-white">
+                      Khoi Nguyen Tran
+                    </a>
+                  </p>
+                  <p>
+                    <a className="text-lg font-semibold text-[#0b1215] dark:text-white">
+                      Dep. of Theoretical Physics
+                    </a>
+                  </p>
+                  <p>
+                    <a className="text-lg font-semibold text-[#0b1215] dark:text-white">
+                      CONTACT ME AT
+                    </a>
+                  </p>
+                  <div className="flex space-x-2">
+                    <a
+                      href="https://jeremy-tran.vercel.app/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className=""
+                    >
+                      <div className="hover:scale-110">
+                        <AiOutlineGlobal className="text-[#0b1215] dark:text-white" />
+                      </div>
+                    </a>
+                    <a
+                      href="https://github.com/nguyn21012003"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      title="github"
+                      data-popover-target="pop"
+                    >
+                      <div className="hover:scale-110">
+                        <FiGithub className="text-[#0b1215] dark:text-white" />
+                      </div>
+                    </a>
+                    <a
+                      href="https://www.facebook.com/nguyn.kool/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      title="facebook"
+                    >
+                      <div className="hover:scale-110">
+                        <FiFacebook className="text-[#0b1215] dark:text-white" />
+                      </div>
+                    </a>
+                    <a
+                      href="https://www.linkedin.com/in/nguyenkool/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      title="Likedin"
+                    >
+                      <div className="hover:scale-110">
+                        <CiLinkedin className="text-[#0b1215] dark:text-white" />
+                      </div>
+                    </a>
                   </div>
-                </a>
-                <a
-                  href="https://github.com/nguyn21012003"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="github"
-                  data-popover-target="pop"
-                >
-                  <div className="hover:scale-110">
-                    <FiGithub />
-                  </div>
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/nguyenkool/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="Likedin"
-                >
-                  <div className="hover:scale-110">
-                      <CiLinkedin />
-                    </div>
-                  </div>
-                </a>
-                <a
-                  href="https://www.facebook.com/nguyn.kool/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="facebook"
-                >
-                  <div className="hover:scale-110">
-                      <FiFacebook />
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div className="space-y-14 p-8">
-              <div>
-                <div className="text-justify font-Poppins text-3xl font-semibold uppercase tracking-wide text-white">
-                  # who am i
-                </div>
-                <p className="text-justify">
-                  <a className="font-Poppins text-lg font-normal leading-tight text-[#7A8BA3] md:max-w-[64rem]">
-                    heoretical Physics, and he is deeply engaged in studying the
-                    subject.Khoi Nguyen Tran is a senior physics student with a
-                    passion for Theoretical Physics, and he is deeply engaged in
-                    studying the subject.Khoi Nguyen Tran is a senior physics
-                    student with a passion for Theoretical Physics, and he is
-                    deeply engaged in studying the subject.Khoi Nguyen Tran is a
-                    senior physics student with a passion for Theoretical
-                    Physics, and he is deeply engaged in studying the subject.
-                    Khoi Nguyen Tran is a senior physics student with a passion
-                    for Theoretical Physics, and he is deeply engaged in
-                    studying the subject.Khoi Nguyen Tran is a senior physics
-                    student with a passion for Theoretical Physics, and he is
-                    deeply engaged in studying the subject.Khoi Nguyen Tran is a
-                    senior physics student with a passion for Theoretical
-                    Physics, and he is deeply engaged in studying the subject.
+                  <div className="flex h-6 w-6 items-center justify-center"></div>
+                  <a
+                    href="/document.pdf"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="border-1 group flex animate-pulse items-center justify-center rounded-full bg-sky-800 p-2 text-base font-normal text-[white] shadow-2xl hover:scale-110 hover:brightness-125 dark:bg-slate-500 dark:hover:brightness-125"
+                  >
+                    <span className="text-center">Check out my CV</span>{" "}
+                    <HiOutlineDocumentDownload className="ml-1" />
                   </a>
-                </p>
+                </div>
+                <div className="space-y-14 p-8">
+                  <div className="space-y-5">
+                    <div className="text-justify text-3xl font-semibold uppercase tracking-wide text-[#0b1215] dark:text-white">
+                      # who am i
+                    </div>
+                    <p className="text-left md:text-justify">
+                      <a className="text-xl font-normal text-[#101720] dark:text-[#7A8BA3] md:max-w-[64em]">
+                        👋 You can call me Jeremy . As you can see, my name is
+                        inspired alot by the character Jeremy Jonhson in Phineas
+                        & Ferb cartoon.
+                      </a>
+                    </p>
+                    <p className="text-left md:text-justify">
+                      <a className="text-xl font-normal text-[#101720] dark:text-[#7A8BA3] md:max-w-[64em]">
+                        Tran Khoi Nguyen aka Khoi Nguyen Tran aka Jeremy is a
+                        senior physics student with a passion not only for
+                        learning Theoretical Physics but also building digital
+                        services/stuff he wants. When not on time 
+                      </a>
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div>
-        <Trimmed></Trimmed>
-      </div>
-
-      <div className="flex items-center justify-center">
-        <Skills></Skills>
-      </div>
-      <div className="flex items-center justify-center pb-[10rem]">
-        <RecentlyPost></RecentlyPost>
+        <div className="flex items-center justify-center">
+          <Skills></Skills>
+        </div>
+        <div className="flex items-center justify-center pb-[10rem]">
+          <RecentlyPost></RecentlyPost>
+        </div>
+        <div className="flex items-center justify-center pb-[10rem]">
+          <MyExperience></MyExperience>
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/extras/layout.tsx
@@ -0,0 +1,3 @@
+export const ExtrasLayout = () => {
+  return <div className="bg-red-50">extras layout</div>;
+};
